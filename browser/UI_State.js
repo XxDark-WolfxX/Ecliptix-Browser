7e6a911244d5315ce5330d3e96cddefaec7bc18c
XxShadow_HackerxX <brittanyburwell19@gmail.com>
2025-12-30 16:02:57 -0500

    Adding in some mandatory files.

---
+++ browser/UI_State.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.tab').forEach(tab => {
+  tab.addEventListener('click', () => {
+    document.querySelectorAll('.tab').forEach(t =>
+      t.classList.remove('active')
+    );
+    tab.classList.add('active');
+  });
+});
